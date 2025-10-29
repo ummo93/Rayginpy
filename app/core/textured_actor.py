bce1e08c03ec96507d0ccf6f465923da739d00f5
Dmitry Luńow <ummo93@gmail.com>
2025-10-29 23:44:45 +0400

    Added textured_actor

--- app/core/textured_actor.py
+++ app/core/textured_actor.py
@@ -1,13 +1,14 @@
-from pyray import Vector2, Texture, draw_texture_ex, WHITE, vector2_subtract
+from pyray import Vector2, Texture, draw_texture_ex, WHITE, vector2_subtract, Color
 
 from app.core.actor import Actor
 
 
 class TexturedActor(Actor):
-    def __init__(self, texture: Texture, scale: float = 1.0, position=Vector2(0, 0), rotation=Vector2(0, 0)):
+    def __init__(self, texture: Texture, scale: float = 1.0, tint: Color = WHITE, position=Vector2(0, 0), rotation=Vector2(0, 0)):
         super().__init__()
         self.texture = texture
         self.scale = scale
+        self.tint = tint
 
     def get_scale(self):
         return Vector2(self.texture.width, self.texture.height)
@@ -18,5 +19,5 @@ class TexturedActor(Actor):
             vector2_subtract(self.position, Vector2((self.texture.width*self.scale)/2, (self.texture.height*self.scale)/2)),
             self.rotation.x,
             self.scale,
-            WHITE
+            self.tint
         )
--- app/game/star.py
+++ app/game/star.py
@@ -5,7 +5,7 @@ from app.core.textured_actor import TexturedActor
 
 class Star(TexturedActor):
     def __init__(self, width: int, gravity: float, color: Color = RED):
-        super().__init__(load_texture("./app/assets/star.png"), 0.25)
+        super().__init__(load_texture("./app/assets/star.png"), 0.25, color)
         self.width = width
         self.gravity = gravity
         self.color = color
